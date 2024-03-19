739c25e33dbe9d1dc8243a4d037835c52a1725fc
Hari097 <harikalam143@gmail.com>
2024-03-19 10:41:21 +0530

    local storage message

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert("welcome javascript")
+let textmessage = document.getElementById("textmessage")
+
+let saveButton = document.getElementById("save") 
+
+let clearButton = document.getElementById("clear")
+
+
+clearButton.onclick =function(){
+    localStorage.removeItem("textmessage")
+    textmessage.value =""
+} 
+
+saveButton.onclick = function(){
+    let message = JSON.stringify(textmessage.value) 
+    localStorage.setItem("textmessage",message)
+     
+} 
+textmessage.textContent = JSON.parse(localStorage.getItem("textmessage"))
